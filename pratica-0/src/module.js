b7583b86f31230239beead9a4c5e44a55f750a73
Leonardo Teodoro <leoteodoro18@gmail.com>
2024-02-26 21:41:39 -0300

    :tada: init: [entrega] pratica-0

---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+    mensagem: 'Testando modulo: Leonardo Teodoro - GEC1753 !',
+    saudacao: function () {
+        console.log(this.mensagem);
+    },
+};
